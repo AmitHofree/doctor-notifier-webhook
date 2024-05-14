4f80a16a6d5ab1b45dd62df3a244369fb270d705
Amit Hofree <amit.hofree@gmail.com>
2024-05-15 00:39:56 +0300

    Implement parameters for register and unregister

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { Bot, CommandContext, Context, webhookCallback } from 'grammy';
 
+type NotificationsRegisteredRow = {
+    chat_id: number;
+    item_key_index: string;
+}
+
 const startCommandReply = `
 Welcome to the Doctor Appointment Notification Bot! 🚑
 
@@ -14,8 +19,8 @@ const helpCommandReply = `
 Doctor Appointment Notification Bot - Command Help 📘
 
 /start - Start interacting with the bot and see this welcome message again.
-/register - Register to receive notifications about new doctor appointments.
-/unregister - Stop receiving notifications about new appointments.
+/register [SERGUIDE_LINK] - Register to receive notifications about new doctor appointments.
+/unregister [SERGUIDE_LINK] - Stop receiving notifications about new appointments.
 /help - Get detailed information about the available bot commands and how to use them.
 
 Just follow the instructions, and I'll handle the rest for you! If you have any questions or encounter any issues, feel free to reach out through this chat.
@@ -29,8 +34,8 @@ export default {
 
 			bot.command('start', (ctx) => ctx.reply(startCommandReply));
 			bot.command('help', (ctx) => ctx.reply(helpCommandReply));
-			bot.command('register', (ctx) => handleRegister(ctx, env));
-			bot.command('unregister', (ctx) => handleUnregister(ctx, env));
+			bot.command('register', (ctx) => ctx.reply(await handleRegister(ctx, env)));
+			bot.command('unregister', (ctx) => ctx.reply(await handleUnregister(ctx, env)))
 
 			const cb = webhookCallback(bot, 'cloudflare-mod');
 			return await cb(request);
@@ -41,36 +46,96 @@ export default {
 	},
 };
 
-async function handleRegister(ctx: CommandContext<Context>, env: Env) {
+async function handleRegister(ctx: CommandContext<Context>, env: Env): Promise<string> {
 	const chatId = ctx.chat.id;
-	const activeChatIds = await getActiveChatIds(env);
-	if (!activeChatIds.includes()) {
-		activeChatIds.push(chatId);
-		await saveActiveChatIds(activeChatIds, env);
-		await ctx.reply('You are now registered for updates.');
+	const item = ctx.match;
+	if (item === undefined) {
+		return 'Please pass a SERGUIDE_LINK to the command to register for that doctor.')
+	}
+	const itemKeyIndex = extractItemKeyIndex(item);
+	if (itemKeyIndex === '') {
+		return 'Unable to extract info from the supplied link. Make sure to pass a link from serguide.maccabi4u.co.il'
+	}
+	const activeChatIds = await getChatIdsForItemKeyIndex(itemKeyIndex, env);
+	if (!activeChatIds.includes(chatId)) {
+		await addChatIdForItemKeyIndex(chatId, itemKeyIndex, env);
+		return 'You are now registered for updates for the specified doctor';
 	} else {
-		await ctx.reply('You are already registered for updates!');
+		return 'You are already registered for updates for the specified doctor!';
 	}
 }
 
-async function handleUnregister(ctx: CommandContext<Context>, env: Env) {
+async function handleUnregister(ctx: CommandContext<Context>, env: Env): Promise<string> {
 	const chatId = ctx.chat.id;
-	const activeChatIds = await getActiveChatIds(env);
-	const index = activeChatIds.indexOf(chatId);
-	if (index > -1) {
-		activeChatIds.splice(index, 1);
-		await saveActiveChatIds(activeChatIds, env);
-		return 'You are now unregistered from updates.';
+	const item = ctx.match;
+	if (item === undefined) {
+		return 'Please pass a SERGUIDE_LINK to the command to unregister for that doctor.')
+	}
+	const itemKeyIndex = extractItemKeyIndex(item);
+	if (itemKeyIndex === '') {
+		return 'Unable to extract info from the supplied link. Make sure to pass a link from serguide.maccabi4u.co.il'
+	}
+	const activeChatIds = await getChatIdsForItemKeyIndex(itemKeyIndex, env);
+	if (activeChatIds.includes(chatId)) {
+		await removeChatIdForItemKeyIndex(chatId, itemKeyIndex, env);
+		return 'You are now unregistered from updates for the specified doctor.';
 	} else {
-		return 'You are not registered for updates.';
+		return 'You are not registered for updates for the specified doctor.';
 	}
 }
 
-async function getActiveChatIds(env: Env) {
-	const data = await env.STORAGE.get('active_chat_ids');
-	return data ? JSON.parse(data) : [];
+async function getChatIdsForItemKeyIndex(itemKeyIndex: string, env: Env): Promise<number[]> {
+	try {
+		const stmt = env.DB.prepare('SELECT chat_id FROM notifications_registered WHERE item_key_index = ?').bind(itemKeyIndex);
+		const { results, success } = await stmt.all<NotificationsRegisteredRow>();
+		if (!success) {
+			console.log("Unknown error executing SQL query in getChatIdsForItemKeyIndex");
+			return [];
+		}
+		return results.map(result => result.chat_id)
+	} catch (e) {
+		if (e instanceof Error)
+			console.error(`Error executing SQL query in getChatIdsForItemKeyIndex - ${e.message}`);
+		return [];
+	}
 }
 
-async function saveActiveChatIds(chatIds: string[], env: Env) {
-	await env.STORAGE.put('active_chat_ids', JSON.stringify(chatIds));
+async function addChatIdForItemKeyIndex(chatId: number, itemKeyIndex: string, env: Env) {
+	try {
+		const stmt = env.DB.prepare('INSERT INTO notifications_registered (chat_id, item_key_index) VALUES = (?, ?)').bind(chatId, itemKeyIndex);
+		const { success } = await stmt.run();
+		if (!success) {
+			console.log("Unknown error executing SQL query in addChatIdForItemKeyIndex");
+		}
+		return;
+	} catch (e) {
+		if (e instanceof Error)
+			console.error(`Error executing SQL query in addChatIdForItemKeyIndex - ${e.message}`);
+		return;
+	}
+}
+
+async function removeChatIdForItemKeyIndex(chatId: number, itemKeyIndex: string env: Env) {
+	try {
+		const stmt = env.DB.prepare('DELETE FROM notifications_registered WHERE chat_id = ? AND item_key_index = ?').bind(chatId, itemKeyIndex);
+		const { success } = await stmt.run();
+		if (!success) {
+			console.log("Unknown error executing SQL query in removeChatIdForItemKeyIndex");
+		}
+		return;
+	} catch (e) {
+		if (e instanceof Error)
+			console.error(`Error executing SQL query in removeChatIdForItemKeyIndex - ${e.message}`);
+		return;
+	}
+}
+
+function extractItemKeyIndex(link: string): string{
+	try {
+		const url = new URL(link);
+		const itemKeyIndex = url.searchParams.get('ItemKeyIndex');
+		return itemKeyIndex ?? '';
+	} catch(TypeError) {
+		return '';
+	}
 }
